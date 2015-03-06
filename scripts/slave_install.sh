409af977cd70c764973437b427a08f6d46c90d14
Thijs Schnitger <thijs@schnitger.org>
2015-03-06 16:02:17 +0100

    Added sudo.

--- scripts/slave_install.sh
+++ scripts/slave_install.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 # Setup
-apt-key adv --keyserver keyserver.ubuntu.com --recv E56151BF
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv E56151BF
 DISTRO=$(lsb_release -is | tr '[:upper:]' '[:lower:]')
 CODENAME=$(lsb_release -cs)
 
 # Add the repository
 echo "deb http://repos.mesosphere.io/${DISTRO} ${CODENAME} main" | \
-  tee /etc/apt/sources.list.d/mesosphere.list
+  sudo tee /etc/apt/sources.list.d/mesosphere.list
 
 #Update the packages
-apt-get -y update
+sudo apt-get -y update
 
 #Install mesos
-apt-get -y install mesos haproxy
+sudo apt-get -y install mesos haproxy

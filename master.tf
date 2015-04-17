467e00c798e5ac91d2ce2bf1d68de9b0a3ab99ec
Martin Westergaard Lassen <martin@mwl.dk>
2015-04-17 10:31:47 +0100

    Installing mesos-dns on all nodes

--- master.tf
+++ master.tf
@@ -39,9 +39,9 @@ resource "google_compute_instance" "mesos-master" {
         "${path.module}/scripts/docker_install.sh",
         "${path.module}/scripts/openvpn_install.sh",
         "${path.module}/scripts/haproxy_marathon_bridge_install.sh",
+        "${path.module}/scripts/mesosdns_install.sh",
         "${path.module}/scripts/common_config.sh",
-        "${path.module}/scripts/master_config.sh",
-        "${path.module}/scripts/mesosdns_install.sh"
+        "${path.module}/scripts/master_config.sh"
       ]
     }
 }
--- scripts/mesosdns_install.sh
+++ scripts/mesosdns_install.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
+MASTERCOUNT=`curl -H "Metadata-Flavor: Google" "http://metadata.google.internal/computeMetadata/v1/instance/attributes/mastercount"`
+CLUSTERNAME=`curl -H "Metadata-Flavor: Google" "http://metadata.google.internal/computeMetadata/v1/instance/attributes/clustername"`
+
+MASTERS_JSON+="[\"${CLUSTERNAME}-mesos-master-0:5050\""
+for ((i=1;i<MASTERCOUNT;i++))
+do
+ MASTERS_JSON+=",\""
+ MASTERS_JSON+=`host ${CLUSTERNAME}-mesos-master-${i}| grep ^${HOSTNAME}| awk '{print $4}'`
+ MASTERS_JSON+=":5050\""
+done
+MASTERS_JSON+=']'
+
 sudo tee /etc/mesos-dns.conf <<JSON
 {
-  "masters": ["127.0.0.1:5050"],
+  "masters": $MASTERS_JSON,
   "refreshSeconds": 60,
   "ttl": 60,
   "domain": "mesos",

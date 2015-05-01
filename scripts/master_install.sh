e7fdd60be0205439a36f6eccdaad603a94540bc5
Thijs Schnitger <thijs@schnitger.org>
2015-05-01 15:12:03 +0200

    first try at source install

--- scripts/master_install.sh
+++ scripts/master_install.sh
@@ -9,8 +9,27 @@ CODENAME=$(lsb_release -cs)
 echo "deb http://repos.mesosphere.io/${DISTRO} ${CODENAME} main" | \
   sudo tee /etc/apt/sources.list.d/mesosphere.list
 
+#Generate the proper locale
+sudo locale-gen nl_NL.UTF-8
+
 #Update the packages
 sudo apt-get -y update
 
 #Install mesos
-sudo apt-get -y install mesosphere haproxy
+#sudo apt-get -y install mesosphere haproxy
+
+#Install from source
+#get dependencies
+sudo apt-get -y install git build-essential openjdk-6-jdk python-dev python-boto libcurl4-nss-dev libsasl2-dev maven libapr1-dev libsvn-dev autoconf libtool
+
+mkdir /tmp/mesos
+cd /tmp/mesos
+git clone https://git-wip-us.apache.org/repos/asf/mesos.git .
+
+./bootstrap
+mkdir build
+cd build
+../configure
+make
+#make check
+sudo make install
